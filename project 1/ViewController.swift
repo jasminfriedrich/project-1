b93d7a2cbd4debd3167db6aeb0911bb87488be7c
Elizabeth Beadle (s5109333) <s5109333@mac38571.mac.bournemouth.ac.uk>
2019-04-02 12:02:40 +0100

    update

--- project 1/ViewController.swift
+++ project 1/ViewController.swift
@@ -1,18 +1,13 @@
-//
-//  ViewController.swift
-//  project 1
-//
-//  Created by Jasmin Friedrich (s5127222) on 02/04/2019.
-//  Copyright © 2019 Jasmin Friedrich (s5127222). All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+     
+        
+        / cccc
+        
     }
 
 
